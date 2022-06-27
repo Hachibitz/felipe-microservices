587dc7eba550ae3a6628729b8ce3c338c2a97fbc
Hachibitz <felipealmeida.ns@outlook.com>
2022-06-27 17:57:13 -0300

    Adding resilience4j and using Retry method

--- 08-api-gateway/src/main/java/br/com/example/filter/LoggingFilter.java
+++ 08-api-gateway/src/main/java/br/com/example/filter/LoggingFilter.java
@@ -17,7 +17,7 @@ public class LoggingFilter implements GlobalFilter{
 	@Override
 	public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
 		//adicionando mensagem de log
-		logger.info("Original request path -> {}", exchange.getRequest().getPath());
+		//logger.info("Original request path -> {}", exchange.getRequest().getPath());
 		return chain.filter(exchange);
 	}
 	
